0aa2f5c68e369dce35c0bdbc0b299c8db2185acc
cacomir1008 <ishimaru1008@gmail.com>
2021-03-21 20:52:34 +0900

    updated

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+// Booklistコンポーネントをimport
+import Booklist from './components/Booklist'; //
+// ライブラリ追加
+import {BrowserRouter, Route, Link}from 'react-router-dom';
+// ライブラリ追加(axios)httpリクエストを送る
+import axios from 'axios';
+<link rel="stylesheet" href="index.css" />
+
+const App = () => {
+  const languages = ['React','Vue','Angular','JavaScript','python','css'];
+  const getDataFromAPI = async keyword => {
+    // volumes ＝検索用のAPIのパス
+    const requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=intitle:'
+    // axios関数＝自動的にpromiseのobjectを返す。awaitの後に書くと、非同期だが同期的に書ける
+    const result = await axios.get(`${requestUrl}${keyword}`);
+    return result;
+  };
+  return(
+    // ルーター(componentの切り替えとURLを関連づける)
+    <BrowserRouter>
+    <div>
+      <h1 style={{color:'#66cdaa'}}>Book list for study</h1>
+      {/* クリックしたら移動する処理（Link） */}
+      <ul style={{display:'flex','list-style':'none'}}>
+        <li><Link to='/'>React</Link></li> <hr />
+        <li><Link to='/vue'>Vue</Link></li> <hr />
+        <li><Link to='/angular'>Angular</Link></li> <hr />
+        <li><Link to='/JavaScript'>JavaScript</Link></li> <hr />
+        <li><Link to='/python'>python</Link></li> <hr />
+        <li><Link to='/css'>CSS</Link></li> <hr />
+      </ul>
+      
+      <hr />
+      {/* URLごとに内容を変える exactつけないと、他のも/で反応してしまう */ }
+      <Route exact path = '/' 
+      render ={
+        props =>
+        // Booklist component呼び出し <Booklist />
+        // componentの中で、propsを通して送りたいデータを指定
+        // component={Booklist}→propsを使いたいので、render={props=>〜}の形に変える
+         <Booklist
+           language ={languages[0]} 
+          // keywordを入力すると、getDataFromApi関数を実行する
+           getData= {keyword=>getDataFromAPI(keyword)}
+           />
+          }
+       />
+      <Route path = '/vue' 
+      render ={
+        props =>
+         <Booklist language ={languages[1]}
+         getData ={keyword=>getDataFromAPI(keyword)}
+        />
+        }
+       />
+      <Route exact path = '/angular' 
+      render ={
+        props =>
+         <Booklist 
+         language ={languages[2]} 
+         getData={keyword=>getDataFromAPI(keyword)}
+         />
+         }
+       />
+        <Route exact path = '/JavaScript' 
+      render ={
+        props =>
+         <Booklist 
+         language ={languages[3]} 
+         getData={keyword=>getDataFromAPI(keyword)}
+         />
+         }
+       />
+        <Route exact path = '/Python' 
+      render ={
+        props =>
+         <Booklist 
+         language ={languages[4]} 
+         getData={keyword=>getDataFromAPI(keyword)}
+         />
+         }
+       />
+        <Route exact path = '/css' 
+      render ={
+        props =>
+         <Booklist 
+         language ={languages[5]} 
+         getData={keyword=>getDataFromAPI(keyword)}
+         />
+         }
+       />
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Booklist.jsx
@@ -0,0 +1,59 @@
+// ★useState=関数componentが値（今回はAPiから取得したデータ）を保管する機能
+// ★useEffect = 関数外の副作用を扱う機能
+// React = Component内でデータ更新＝＞再レンダリング＝APIからデータ取得＞無限ループ
+// レンダリングを制御、特定の値が更新された時のみ処理を実行させる（今回はpropsが更新）
+import React, { useState, useEffect}from 'react';
+<link rel="stylesheet" href="./index.css" />
+
+// props 親から子にデータを渡す時に使う。親からのデータは全部propsに自動的に入ってくる。
+// データ取り出す：props.名前
+// 中にjavascriptを書いて、親から来たデータの処理に使うこともできる
+// コンポーネントは2種類（class：stateful/関数：stateless）
+// ▼関数コンポーネント（状態を持つことができない）React hooksを使えば可能
+// 状態（ステート）を持つと重くなる＝基本は関数コンポーネントを使う
+const Booklist = props =>{
+  // useState ：書き方　配列に2つ値が入っている状態にする
+  // bookData：データ保存用（配列）
+  // setBookData = bookDataの値を更新する関数
+  // bookDataの初期値設定（null）
+    const [bookData, setBookData]= useState(null)
+    
+    useEffect(() => {
+      // ?＝getDataが存在する場合のみ関数を実行できる（エラー防止）
+      // ▼一番最初の1回と、propsに変化があった時のみ実行される書き方（無限ループ避け）
+        const result = props
+        .getData?.(props.language)
+        // responseにAPIからのデータが入る→BookData（データ保存）へ入れる
+        .then((response) => setBookData(response));
+    },[props]);                                                                                             
+    return(
+        <div>
+          <ul style={{'list-style':'square'}}>
+          {/* 三項演算子 */}
+        {
+          bookData === null 
+          ? (
+            <p>now loading...</p>
+          ) : 
+          (
+            // bookDataにデータが入って来たら下記の処理
+            // 繰り返し処理 map関数＝配列内の要素をコールバック処理、配列としてreturn
+            console.dir(bookData.data.items),
+            bookData.data.items.map((item1,index) => (
+              <li key={index}>
+                <h2 style={{color:'skyblue'}}>「{item1.volumeInfo.title}」</h2>
+                <p><img src ={item1.volumeInfo.imageLinks ? item1.volumeInfo.imageLinks.smallThumbnail:""}></img></p>
+                <p className="text">著者：{item1.volumeInfo.authors}</p>
+                <p>詳細：{item1.volumeInfo.description}</p>
+                <a href ={item1.volumeInfo.canonicalVolumeLink}>{item1.volumeInfo.canonicalVolumeLink}</a>
+              </li>
+            ))
+          )
+        }
+          </ul>
+        </div>
+    );
+}
+
+// 他のファイルからコンポーネントを呼び出せるように
+export default Booklist;
